05a4a3579000cf9eb9281f5496654edc5e1c882e
Augusto Costa <augustogiles@gmail.com>
2020-04-18 03:33:50 +0100

    Updates

---
+++ precache-manifest.d4bf29ba3229fc5c00088a275c65b7c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23643ac3a201ff1ac223a6e6afde09d",
+    "url": "/landing-page/index.html"
+  },
+  {
+    "revision": "c6d83287c326a6ad2abb",
+    "url": "/landing-page/static/css/main.2eb7e82b.chunk.css"
+  },
+  {
+    "revision": "981fb889b99c3c2463cb",
+    "url": "/landing-page/static/js/2.74453d29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing-page/static/js/2.74453d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d83287c326a6ad2abb",
+    "url": "/landing-page/static/js/main.b48cebde.chunk.js"
+  },
+  {
+    "revision": "d2db949335bfbcbae226",
+    "url": "/landing-page/static/js/runtime-main.fa118eef.js"
+  },
+  {
+    "revision": "8b04890cde2c37f4fb1a8e9c2d589c95",
+    "url": "/landing-page/static/media/resume.8b04890c.pdf"
+  }
+]);
